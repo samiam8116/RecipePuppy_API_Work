44c2dd9260feee865db03bf579a1ddea96db562f
Sam Silvia <s5silvia@student.bridgew.edu>
2020-05-26 16:07:05 -0400

    Added function calls to main

---
+++ GetRecipeData.py
@@ -0,0 +1,14 @@
+import requests
+
+def display_data(to_display):
+    pass
+
+def get_data():
+    pass
+
+def main():
+    data = get_data()
+    display_data(data)
+
+if __name__ == '__main__ ':
+    main()
