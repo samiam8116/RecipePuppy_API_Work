bf520073ed2a36b0cc4c78622230009449359783
Sam Silvia <s5silvia@student.bridgew.edu>
2020-05-28 10:58:41 -0400

    Add API code

--- GetRecipeData.py
+++ GetRecipeData.py
@@ -1,14 +1,32 @@
 import requests
 
-def display_data(to_display):
-    pass
 
-def get_data():
-    pass
+def display_data(to_display):
+    for recipe in to_display:
+        print(recipe['title'])
+
+
+def get_data(location):
+    response = requests.get(location)
+    if response.status_code != 200:
+        return dict()
+    data = response.json()
+    return data["results"]
+
+
+def get_params():
+    ingredients = input("what ingredients shall we include? (comma separated):")
+    recipe_type = input("what recipe shall we look for?:")
+    return ingredients, recipe_type
+
 
 def main():
-    data = get_data()
+    params = get_params()
+    loc = f"http://www.recipepuppy.com/api/?i={params[0]}&q={params[1]}"
+    print(loc)
+    data = get_data(loc)
     display_data(data)
+
 
 if __name__ == '__main__ ':
     main()
